7e9925b40c1573c38263a8a811f53ba4d9cc9f13
Administrador <viniciuscorrea417@gmail.com>
2024-03-21 12:22:42 -0300

    Activation Softmax and ReLU / Dropout and Input Layer

--- src/model/model.py
+++ src/model/model.py
@@ -51,5 +51,70 @@ class Layer_Dense:
 
 
 
+class Layer_Dropout:
+
+    def __init__(self, rate):
+        self.rate = 1 - rate
+
+    def forward(self, inputs, training):
+
+
+        self.inputs = inputs
+
+        if not training:
+            self.output = inputs.copy()
+
+        self.binary_mask = np.random.binomial(1, self.rate, size=inputs.shape)/self.rate
+
+        self.output = inputs * self.binary_mask
+
+    def backward(self,dvalues):
+        self.dinputs = dvalues * self.binary_mask
+
+
+class Layer_Input:
+
+    def forward(self, inputs):
+        self.output = inputs
+
+class Activation_ReLU:
+
+    def forward(self, inputs, training):
+        self.inputs = inputs
+        self.output = np.maximun(0, inputs)
+
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copu()
+        self.dinputs[self.inputs <= 0] = 0
+
+
+    def predictions(self, outputs):
+        return outputs
+
+class Activation_Softmax:
+
+    def forward(self, inputs, training):
+
+        self.inputs = inputs
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+
+        prob = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
+        self.output = prob
+
+    def backward(self, dvalues):
+        self.dinputs = np.empty_like(dvalues)
+
+        for i, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
+
+            single_output = single_output.reshape(-1, 1)
+            jacobian_mtx = np.diagflat(single_output) - np.dot(single_output, single_output.T)
+
+            self.dinputs[i] = np.dot(jacobian_mtx, single_dvalues)
+
+
+
+    def prediction(self, outputs):
+        return np.argmax(outputs, axis=1)
 
 
